8fc154fb016de3f5cf0056d6ae318c630992779f
luiscunhacsc <93707203+luiscunhacsc@users.noreply.github.com>
2025-02-19 15:18:42 +0000

    Adicionado modificador private

---
+++ Exemplo 01 - Uma Classe com Campos Privados/Carro.java
@@ -0,0 +1,6 @@
+public class Carro {
+
+    private String marca;
+    private String modelo;
+
+}
---
+++ Exemplo 01 - Uma Classe com Campos Privados/StaterLuis.java
@@ -0,0 +1,21 @@
+public class StaterLuis {
+
+    public static void main(String[] args) {
+
+        Carro carroDaMulherDoLuis = new Carro();
+        Carro carroDoLuis = new Carro();
+        Carro carroDoTomas = new Carro();
+
+        carroDaMulherDoLuis.marca = "Citroen";
+        carroDaMulherDoLuis.modelo = "C3";
+
+        carroDoLuis.marca = "Suzuki";
+        carroDoLuis.modelo = "Grand Vitara";
+
+        carroDoTomas.marca = "Opel";
+        carroDoTomas.modelo = "Corsa";
+        
+
+    }
+    
+}
